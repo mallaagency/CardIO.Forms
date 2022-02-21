dc9651f406c99549b904650d4c0c91e48a9ab5fd
luismts <deivi_0326@hotmail.com>
2022-02-21 18:56:46 -0400

    Add sample

--- samples/MallaCardIOSample/MallaCardIOSample/MallaCardIOSample/MainPage.xaml.cs
+++ samples/MallaCardIOSample/MallaCardIOSample/MallaCardIOSample/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Malla.CardIO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,18 @@ namespace MallaCardIOSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var config = new CardIOConfig
+            {
+                Localization = "es",
+                ShowPaypalLogo = false
+            };
+
+            var result = await CardIO.Instance.Scan(config);
+            await DisplayAlert("", $"Card: {result.CardNumber} /n Date: {result.Expiry}", "Ok");
         }
     }
 }
