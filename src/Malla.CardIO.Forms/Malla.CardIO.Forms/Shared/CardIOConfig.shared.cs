a3b05c090f5603944af28090ec28ef4f66333a01
luismts <deivi_0326@hotmail.com>
2022-02-22 02:00:47 -0400

    Add extra config

--- src/Malla.CardIO.Forms/Malla.CardIO.Forms/Shared/CardIOConfig.shared.cs
+++ src/Malla.CardIO.Forms/Malla.CardIO.Forms/Shared/CardIOConfig.shared.cs
@@ -12,13 +12,32 @@ namespace Malla.CardIO
     public class CardIOConfig
     {
         /// <summary>
+        /// Creates a new instance of <c>CardIOConfig</c>
+        /// </summary>
+        public CardIOConfig()
+        {
+            this.ShowPaypalLogo = true;
+            this.ShowCardIOLogo = true;
+            this.RequireExpiry = true;
+            this.RequireCvv = false;
+            this.RequirePostalCode = false;
+        }
+
+        /// <summary>
         /// Gets/Sets if expiry is required
         /// </summary>
         public bool RequireExpiry { get; set; }
+
+        /// <summary>
+        /// Gets/Sets if scan expiry
+        /// </summary>
+        public bool ScanExpiry { get; set; }
+
         /// <summary>
         /// Gets/Sets if CVV is required
         /// </summary>
         public bool RequireCvv { get; set; }
+
         /// <summary>
         /// Gets/Sets if postal code is required
         /// </summary>
@@ -28,25 +47,37 @@ namespace Malla.CardIO
         /// Gets/Sets if the PayPal Logo should be displayed
         /// </summary>
         public bool ShowPaypalLogo { get; set; }
+
+        /// <summary>
+        /// Gets/Sets if the CardIO Logo should be displayed
+        /// </summary>
+        public bool ShowCardIOLogo { get; set; }
+
+        /// <summary>
+        /// Gets/Sets if supress manual entry
+        /// </summary>
+        public bool SuppressManualEntry { get; set; }
+
+        /// <summary>
+        /// Gets/Sets if supress confirmation
+        /// </summary>
+        public bool SuppressConfirmation { get; set; }
+
         /// <summary>
         /// Gets/Sets the scan instructions displayed while scanning
         /// </summary>
         public string ScanInstructions { get; set; }
+
         /// <summary>
         /// Gets/Sets the localization
         /// Currently supported localizations: ar,da,de,en,en_AU,en_GB,es,es_MX,fr,he,is,it,ja,ko,ms,nb,nl,pl,pt,pt_BR,ru,sv,th,tr,zh-Hans,zh-Hant,zh-Hant_TW.
         /// </summary>
         public string Localization { get; set; }
+
         /// <summary>
-        /// Creates a new instance of <c>CardIOConfig</c>
+        /// Gets/Sets if return card image
         /// </summary>
-        public CardIOConfig()
-        {
-            this.ShowPaypalLogo = true;
-            this.RequireExpiry = true;
-            this.RequireCvv = false;
-            this.RequirePostalCode = false;
-        }
+        //public bool ReturnCardImage { get; set; }
         
     }
 }
