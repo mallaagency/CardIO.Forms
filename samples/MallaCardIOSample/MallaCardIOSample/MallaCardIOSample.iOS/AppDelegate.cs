2fd7dfd92e2e79418efaeffb77668645ae5118cb
luismts <deivi_0326@hotmail.com>
2022-02-26 15:38:24 -0400

    Add init method

--- samples/MallaCardIOSample/MallaCardIOSample/MallaCardIOSample.iOS/AppDelegate.cs
+++ samples/MallaCardIOSample/MallaCardIOSample/MallaCardIOSample.iOS/AppDelegate.cs
@@ -23,6 +23,8 @@ namespace MallaCardIOSample.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Malla.CardIO.iOS.CardIO.Init();
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
